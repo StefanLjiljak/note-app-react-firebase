5089030713a79f92c634b7b5e91936357fc87f05
StefanLjiljak <stefanljiljak@gmail.com>
2020-07-31 17:51:46 +0200

    docs folder added

---
+++ docs/precache-manifest.6b697858e607db41a65c54beaee952af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02badf4a620fcf99b7cbfebdbe8966bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a2a4ed004d3e30291f9",
+    "url": "/static/css/main.2e7a69b9.chunk.css"
+  },
+  {
+    "revision": "e79802d0fcefcddb8962",
+    "url": "/static/js/2.cc48a10b.chunk.js"
+  },
+  {
+    "revision": "e66d8bfcf01b9d69124b67e3e620fab0",
+    "url": "/static/js/2.cc48a10b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a2a4ed004d3e30291f9",
+    "url": "/static/js/main.0ef7eb60.chunk.js"
+  },
+  {
+    "revision": "a283ce4fdf014de6136b",
+    "url": "/static/js/runtime-main.6ec909cc.js"
+  }
+]);
